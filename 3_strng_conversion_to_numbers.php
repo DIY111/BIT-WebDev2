e46fc3c2b2cc5ba32820284d20b30b1a9c70ba79
Shiham Samsudeen <shihams247@gmail.com>
2018-01-20 18:00:36 +0530

    string conversion to numbers

---
+++ 3_strng_conversion_to_numbers.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+  
+    <title>Hello</title>
+</head>
+<body>
+    <h1>String Conversion to Numbers !!!</h1>
+
+    <?php 
+
+        echo "<pre>";
+        
+        echo "1-", "10 " > 1 ,"\n";
+        echo "2-", "10 items " > 2 ,"\n";
+        echo "3-", "items " > 2 , "\n";
+        echo "4-", True == 1, "\n";
+        echo "5-", false == 0, "\n";
+        echo "6-", TRUE == FALSE, "\n";
+
+        echo "</pre>";
+
+    
+    ?>
+    
+</body>
+</html>
