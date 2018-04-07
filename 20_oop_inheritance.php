1f379b2b7d74a9b3799fcdb26cd5097acb094f64
Shiham Samsudeen <shihams247@gmail.com>
2018-04-08 02:40:58 +0530

    comments added

--- 20_oop_inheritance.php
+++ 20_oop_inheritance.php
@@ -12,13 +12,14 @@ class Circle extends Shape // inheritance using the extends keyword
 {
     public $radius;
 
+    // this method overrides the functionality of the Shape(parent) class
     public function Hello(){
         echo "Hello from Cirle";
     }
 }
 
 $c = new Circle();
-print_r($c->center);
+//print_r($c->center);
 
 $c->Hello();
 
