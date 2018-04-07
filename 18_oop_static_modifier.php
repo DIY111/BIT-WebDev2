787765f21e9ed9a2ed9f0d66d05f1b39a70c5851
Shiham Samsudeen <shihams247@gmail.com>
2018-04-08 02:38:42 +0530

    static modifier comments added

--- 18_oop_static_modifier.php
+++ 18_oop_static_modifier.php
@@ -4,21 +4,38 @@ class Person{
     public $sex = "m";
     private static $objectCount = 0;
 
-    function __construct($name, $sex){
+    public function __construct($name, $sex){
         $this->name = $name;
         $this->sex = $sex;
         self::$objectCount++;
+        //echo "constructor values ".  self::$objectCount . "<br>";
+
     }
 
     function print_object_count(){
-        echo "Number of objects instantiated - " . self::$objectCount;
+        //echo "Number of objects instantiated : " . self::$objectCount;
     }   
 } // end of Class definition
 
-$p1 = new Person("Saman", "m");
-$p2 = new Person("Kamla", "f");
+$p1 = new Person("Nimal", "m"); 
 
+$p2 = new Person("Kamal", "m"); 
+
+$p3 = new Person("Sunil", "m");
+
+$p4 = new Person("Vimal", "m");
+
+// print the object count
 Person::print_object_count();
+
+//assign values to a static variable
+Person::$objectCount = 6;
+
+echo Person::$objectCount;
+
+
+
+
 
 
 ?>
