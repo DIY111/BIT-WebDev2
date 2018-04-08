b33945d23696c41893a9d6807f749f1397bd676a
Shiham Samsudeen <shihams247@gmail.com>
2018-04-08 07:38:39 +0530

    magic methods update

--- 24_magic_methods.php
+++ 24_magic_methods.php
@@ -1,10 +1,21 @@
 <?php 
     // __call, __get, __set, __toString
+class Test{
 
     function __call($m, $a){
         echo "The method " . $m . " was called. <br> The arguments were as follows<br>";
         print_r($a);
     }
     
+    function Hello(){
+        echo "Hello Universe";
+    
+    }
+}
+
+    $test = new Test();
+
+    // calling a method that does not exist
+    $test->HelloBro();
 
 ?>
